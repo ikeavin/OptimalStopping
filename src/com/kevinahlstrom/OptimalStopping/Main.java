8201a732e54ab97051e2a34ea112554a131e815a
Kevin Ahlstrom <ikeavin@gmail.com>
2020-12-10 12:56:24 -0700

    Created StopObject class and finished basic features

--- src/com/kevinahlstrom/OptimalStopping/Main.java
+++ src/com/kevinahlstrom/OptimalStopping/Main.java
@@ -1,18 +1,61 @@
 package com.kevinahlstrom.OptimalStopping;
 
 import java.awt.Desktop;
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Collections;
 
 public class Main {
 
 	
-	public static void main(String[] args) throws IOException, URISyntaxException {
+	public static void main(String[] args) throws IOException {
+		Scanner resources = new Scanner(new File("resources.txt"));
+		Scanner input = new Scanner(System.in);
 		
-		//Add command line functionality
+		List<StopObject> stopObjects = new ArrayList<StopObject>();
+		while(resources.hasNext()) {
+			stopObjects.add(new StopObject(resources.next()));
+		}
+		Collections.shuffle(stopObjects);
+		float scoreTarget = -Float.MAX_VALUE;
+		int earliestStopIndex = (int) Math.floor((stopObjects.size() - 1) * .37);
+		StopObject decision;
+		
+		for(int i = 0; i < stopObjects.size(); i++) {
+			if(stopObjects.get(i).isUrl) {
+				System.out.println("Opening url '" + stopObjects.get(i).name + "'...");
+				try {
+					openUrl(stopObjects.get(i).name);
+				} catch(Exception e) {
+					System.out.println("Url could not be open. ");
+				}
+			} else {
+				System.out.println("Item " + (i+1) + ": " + stopObjects.get(i).name);
+			}
+			System.out.println("Enter score value: ");
+			Float score = input.nextFloat();
+			stopObjects.get(i).setScore(score);
+			
+			if(score > scoreTarget) {
+				scoreTarget = score;
+				decision = stopObjects.get(i);
+				if(i >= earliestStopIndex) {
+					System.out.println("Decision made: " + decision.name);
+					break;
+				}
+			}
+		}
+		
+	}
+	
+	private static void openUrl(String url) throws IOException, URISyntaxException {
 		if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
-		    Desktop.getDesktop().browse(new URI("http://www.google.com"));
+		    Desktop.getDesktop().browse(new URI(url));
 		}
 	}
 }
--- src/com/kevinahlstrom/OptimalStopping/StopObject.java
+++ src/com/kevinahlstrom/OptimalStopping/StopObject.java
@@ -2,4 +2,46 @@ package com.kevinahlstrom.OptimalStopping;
 
 public class StopObject {
 
+	public String name;
+	public boolean isUrl;
+	public float score;
+	
+	//Various constructors, name is the only parameter always required
+	public StopObject(String name) {
+		this.name = name;
+		this.isUrl = isUrl();
+	}
+	
+	public StopObject(String name, boolean isUrl) {
+		this.name = name;
+		this.isUrl = isUrl;
+	}
+	
+	public StopObject(String name, boolean isUrl, float score) {
+		this.name = name;
+		this.isUrl = isUrl;
+		this.score = score;
+	}
+	
+	public StopObject(String name, float score) {
+		this.name = name;
+		this.isUrl = isUrl();
+		this.score = score;
+	}
+	
+	private boolean isUrl() {
+		if(this.name.contains("http") && this.name.contains("://")) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public void setScore(float score) {
+		this.score = score;
+	}
+	
+	public void standardizeScore(float standard) {
+		this.score = score/standard ;
+	}
 }
